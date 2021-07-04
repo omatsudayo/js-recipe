8d103cf3fb863ba8738c9e68157f4155b39df546
omatsudayo <matsuri.motoki.5c@stu.hosei.ac.jp>
2021-07-04 21:00:18 +0900

    3のばいすうをやった

---
+++ 04_ゼミ/week1/objrct.js
@@ -0,0 +1,19 @@
+const Matsurin = {
+    name: 'まつりん',
+    university: '法政大学',
+    age: 20,
+    hobby: ['ダンス', 'マリカ'],
+    birthplace: "東京"
+    familly: {
+      papa: 'こーじ',
+      mama: 'ゆーり',
+      sis: 'りずむ',
+    },
+    isMentor: true,
+    sayHello: function () {
+      alert(`こんにちは${this.name}です!!`)
+    },
+  }
+  ​
+  console.log(Name.name)
+  Name.sayHello()
